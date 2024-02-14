a68bd61b80c391237fe4df76087d8464655c33f6
Jn-Sajol <julkar10121@gmail.com>
2024-02-14 13:36:32 +0600

    adding filter segment

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,48 +1,82 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final List<String> filters = ['all', 'nike', 'adidas', 'lotto','bata','walker'];
+
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Padding(
-        padding: EdgeInsets.only(left: 44),
+        padding: EdgeInsets.all(12),
         child: Scaffold(
-            body: Column(
-          children: [
-            Row(
-              // mainAxisAlignment = MainAxisAlignment.start,
-              children: [
-                //app logo
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.all(6),
-                    child: Text('Shoe \n BestShop',style: TextStyle(fontWeight:FontWeight.bold,fontSize: 25),),
-                  ),
-                ),
-
-                //search field
-                Expanded(
-                  child: TextField(
-                    // expands: true,
-                    decoration: InputDecoration(
-                      hintText: 'Search',
-                      prefixIcon: Icon(
-                        Icons.search,
-                        color: Colors.black,
+          body: Column(
+            children: [
+              const Row(
+                // mainAxisAlignment = MainAxisAlignment.start,
+                children: [
+                  //app logo
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 26),
+                      child: Text(
+                        'Shoe \n BestShop',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 25),
                       ),
-                      border: OutlineInputBorder(
-                          // borderRadius = const BorderRadius.all(Radius.circular(4.0))
-                        borderRadius: BorderRadius.only(topLeft: Radius.circular(12),bottomLeft: Radius.circular(12))
-                      )
                     ),
                   ),
+
+                  //search field
+                  Expanded(
+                    child: TextField(
+                      // expands: true,
+                      decoration: InputDecoration(
+                          hintText: 'Search',
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Colors.black,
+                          ),
+                          border: OutlineInputBorder(
+                              // borderRadius = const BorderRadius.all(Radius.circular(4.0))
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(12),
+                                  bottomLeft: Radius.circular(12)))),
+                    ),
+                  ),
+                ],
+              ),
+              //Start Filtering Section
+              SizedBox(
+                height: 100,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: filters.length,
+                    itemBuilder:(context,index){
+                    final filter = filters[index];
+                    // print(filter);
+                    return  Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Chip(
+                        label: Text(filter),
+                        backgroundColor: Colors.black26,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          side: BorderSide(
+                            color: Colors.green,
+                          )
+                        )
+                        // elevation: 2,
+                      ),
+                    );
+                    },
                 ),
-              ],
-            )
-          ],
-        )),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
