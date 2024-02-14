7f62e96b3dbcc9f6dff9a718282878a906d6203a
Jn-Sajol <julkar10121@gmail.com>
2024-02-14 11:43:37 +0600

    header completed

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,8 +5,45 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('Eccomerce app'),
+    return const SafeArea(
+      child: Padding(
+        padding: EdgeInsets.only(left: 44),
+        child: Scaffold(
+            body: Column(
+          children: [
+            Row(
+              // mainAxisAlignment = MainAxisAlignment.start,
+              children: [
+                //app logo
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(6),
+                    child: Text('Shoe \n BestShop',style: TextStyle(fontWeight:FontWeight.bold,fontSize: 25),),
+                  ),
+                ),
+
+                //search field
+                Expanded(
+                  child: TextField(
+                    // expands: true,
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.black,
+                      ),
+                      border: OutlineInputBorder(
+                          // borderRadius = const BorderRadius.all(Radius.circular(4.0))
+                        borderRadius: BorderRadius.only(topLeft: Radius.circular(12),bottomLeft: Radius.circular(12))
+                      )
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        )),
+      ),
     );
   }
 }
