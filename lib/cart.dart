2515b36f879bd65e3652d1985c86ef94e29ca81f
Jn-Sajol <julkar10121@gmail.com>
2024-02-18 11:56:05 +0600

    buiding cart style

--- lib/cart.dart
+++ lib/cart.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/global_shop_data.dart';
 import 'package:flutter/material.dart';
 
 class Cart extends StatelessWidget {
@@ -7,8 +8,26 @@ class Cart extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Text'),
+        title:const  Text('Text'),
         centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: cart.length,
+        itemBuilder: (context, index) {
+          var item = cart[index];
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage(item['imageUrl'].toString()),
+              // radius: 100,
+            ),
+            title: Text(item['title'].toString()),
+            subtitle:Text(item['company'].toString()),
+            trailing: IconButton(
+              onPressed: (){},
+              icon:const  Icon(Icons.delete,color: Colors.red,),
+            ),
+          );
+        },
       ),
     );
   }
